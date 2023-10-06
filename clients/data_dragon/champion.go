ad104e4544d53a41992ec17ca00cf3461bb7110d
Kyagara <kevinsouzarocha@outlook.com>
2023-10-06 11:29:16 -0300

    updating data dragon champion model

--- clients/data_dragon/champion.go
+++ clients/data_dragon/champion.go
@@ -13,12 +13,13 @@ type ChampionEndpoint struct {
 }
 
 type ChampionData struct {
-	ID    string `json:"id"`
-	Key   string `json:"key"`
-	Name  string `json:"name"`
-	Title string `json:"title"`
-	Blurb string `json:"blurb"`
-	Info  struct {
+	Version string `json:"version"`
+	ID      string `json:"id"`
+	Key     string `json:"key"`
+	Name    string `json:"name"`
+	Title   string `json:"title"`
+	Blurb   string `json:"blurb"`
+	Info    struct {
 		Attack     int `json:"attack"`
 		Defense    int `json:"defense"`
 		Magic      int `json:"magic"`
@@ -46,51 +47,9 @@ type ChampionData struct {
 		CritPerLevel         float64 `json:"critperlevel"`
 		AttackDamage         float64 `json:"attackdamage"`
 		AttackDamagePerLevel float64 `json:"attackdamageperlevel"`
-		AttackSpeedOffset    float64 `json:"attackspeedoffset"`
 		AttackSpeedPerLevel  float64 `json:"attackspeedperlevel"`
+		AttackSpeed          float64 `json:"attackspeed"`
 	} `json:"stats"`
-	Skins []struct {
-		ID      string `json:"id"`
-		Num     int    `json:"num"`
-		Name    string `json:"name"`
-		Chromas bool   `json:"chromas"`
-	} `json:"skins"`
-	Lore      string   `json:"lore"`
-	AllyTips  []string `json:"allytips"`
-	EnemyTips []string `json:"enemytips"`
-	Spells    []struct {
-		ID          string `json:"id"`
-		Name        string `json:"name"`
-		Description string `json:"abstract"`
-		Tooltip     string `json:"tooltip"`
-		Leveltip    struct {
-			Label  []string `json:"label"`
-			Effect []string `json:"effect"`
-		} `json:"leveltip"`
-		MaxRank      int         `json:"maxrank"`
-		Cooldown     []float64   `json:"cooldown"`
-		CooldownBurn string      `json:"cooldownBurn"`
-		Cost         []float64   `json:"cost"`
-		CostBurn     string      `json:"costBurn"`
-		Effect       [][]float64 `json:"effect"`
-		EffectBurn   []string    `json:"effectBurn"`
-		Vars         []struct {
-			Link        string  `json:"link"`
-			Coefficient float64 `json:"coeff"`
-			Key         string  `json:"key"`
-		} `json:"vars"`
-		CostType  string    `json:"costType"`
-		MaxAmmo   string    `json:"maxammo"`
-		Range     []float64 `json:"range"`
-		RangeBurn string    `json:"rangeBurn"`
-		Image     Image     `json:"image"`
-		Resource  string    `json:"resource"`
-	} `json:"spells"`
-	Passive struct {
-		Name        string `json:"name"`
-		Description string `json:"description"`
-		Image       Image  `json:"image"`
-	} `json:"passive"`
 }
 
 type Image struct {
