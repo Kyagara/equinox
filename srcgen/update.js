b018f4a4cf70f2c811eee8822269a9ea929566f5
Kyagara <kevinsouzarocha@outlook.com>
2023-11-11 18:00:36 -0300

    LobbyEventsWrapper name change, spec update

--- srcgen/update.js
+++ srcgen/update.js
@@ -3,13 +3,13 @@ const fetch = require('node-fetch-commonjs')
 process.chdir(__dirname)
 
 const files = [
-  ['http://www.mingweisamuel.com/riotapi-schema/openapi-3.0.0.json', './specs/.spec.json'],
-  ['http://www.mingweisamuel.com/riotapi-schema/enums/queues.json', './specs/.queues.json'],
-  ['http://www.mingweisamuel.com/riotapi-schema/enums/queueTypes.json', './specs/.queueTypes.json'],
-  ['http://www.mingweisamuel.com/riotapi-schema/enums/gameTypes.json', './specs/.gameTypes.json'],
-  ['http://www.mingweisamuel.com/riotapi-schema/enums/gameModes.json', './specs/.gameModes.json'],
-  ['http://www.mingweisamuel.com/riotapi-schema/enums/maps.json', './specs/.maps.json'],
-  ['http://www.mingweisamuel.com/riotapi-schema/routesTable.json', './specs/.routesTable.json'],
+  ['http://www.mingweisamuel.com/riotapi-schema/openapi-3.0.0.json', './specs/spec.json'],
+  ['http://www.mingweisamuel.com/riotapi-schema/enums/queues.json', './specs/queues.json'],
+  ['http://www.mingweisamuel.com/riotapi-schema/enums/queueTypes.json', './specs/queueTypes.json'],
+  ['http://www.mingweisamuel.com/riotapi-schema/enums/gameTypes.json', './specs/gameTypes.json'],
+  ['http://www.mingweisamuel.com/riotapi-schema/enums/gameModes.json', './specs/gameModes.json'],
+  ['http://www.mingweisamuel.com/riotapi-schema/enums/maps.json', './specs/maps.json'],
+  ['http://www.mingweisamuel.com/riotapi-schema/routesTable.json', './specs/routesTable.json'],
 ]
 
 if (!fs.existsSync('./specs')) {
