b4fbd5f1c634e96f68101a990fe6c9696e899c22
Kyagara <kyagara@proton.me>
2023-12-04 16:33:31 -0300

    moving checkBuckets to Limit struct

--- ratelimit/limit.go
+++ ratelimit/limit.go
@@ -1,9 +1,13 @@
 package ratelimit
 
 import (
+	"context"
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/Kyagara/equinox/api"
+	"go.uber.org/zap"
 )
 
 // Limit represents a collection of buckets and the type of limit (application or method).
@@ -19,6 +23,29 @@ func NewLimit(limitType string) *Limit {
 		limitType: limitType,
 		mutex:     sync.Mutex{},
 	}
+}
+
+// Checks if any of the buckets provided are rate limited, and if so, blocks until the next reset.
+func (l *Limit) checkBuckets(ctx context.Context, equinoxReq *api.EquinoxRequest) error {
+	var limited []*Bucket
+	for _, bucket := range l.buckets {
+		if bucket.isRateLimited() {
+			limited = append(limited, bucket)
+		}
+	}
+	for i := len(limited) - 1; i >= 0; i-- {
+		equinoxReq.Logger.Warn("Rate limited", zap.String("limit_type", l.limitType), zap.Any("route", equinoxReq.Route), zap.Object("bucket", limited[i]))
+		err := limited[i].wait(ctx)
+		if err != nil {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			default:
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 // Checks if the limits given in the header match the current buckets.
