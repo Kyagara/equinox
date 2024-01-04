4ed05e10547de8c9a02e714f97d4fad2ff594e43
Kyagara <kyagara@proton.me>
2024-01-04 19:06:39 -0300

    Logger comment

--- internal/logger.go
+++ internal/logger.go
@@ -41,7 +41,7 @@ func NewLogger(config api.EquinoxConfig) zerolog.Logger {
 	return logger
 }
 
-// Used to access the internal logger, this is used to log events from other clients (RiotClient, LOLClient...).
+// Used to access the internal logger, this is used to log events from other clients.
 func (c *Client) Logger(id string) zerolog.Logger {
 	c.loggers.mutex.Lock()
 	defer c.loggers.mutex.Unlock()
