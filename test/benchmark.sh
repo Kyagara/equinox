0fa6abdb88374d02d9f98c794e2a4a8ccf189b82
Kyagara <kyagara@proton.me>
2024-05-03 22:19:05 -0300

    bash script to generate benchmark results table

---
+++ test/benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "| Benchmark | ops | ns/op | bytes/op | allocs/op |"
+echo "|-----------|-----|-------|----------|-----------|"
+
+while read -r line; do
+    if [[ $line =~ Benchmark.*-16 ]]; then
+        benchmark=$(echo "$line" | awk '{print $1}' | awk '{sub(/Benchmark/,""); print}')
+        ops=$(echo "$line" | awk '{print $2}')
+        time=$(echo "$line" | awk '{print $3}')
+        bytes=$(echo "$line" | awk '{print $5}')
+        allocs=$(echo "$line" | awk '{print $7}')
+
+        echo "| $benchmark | $ops | $time | $bytes | $allocs |"
+    fi
+done
