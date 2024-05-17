11d97a35f81a8a7843cafc2bcaa819b94ad790ab
Kyagara <kevinsouzarocha@outlook.com>
2024-05-17 11:20:11 -0300

    updating specs

--- clients/val/client.go
+++ clients/val/client.go
@@ -15,7 +15,7 @@ package val
 //                                           //
 ///////////////////////////////////////////////
 
-// Spec version = 6461993a9c4165ddca053929f19f6d0e3eb1ca14
+// Spec version = 26952273dd9de767dc805b41d363fe8ff8cd0510
 
 import "github.com/Kyagara/equinox/v2/internal"
 
